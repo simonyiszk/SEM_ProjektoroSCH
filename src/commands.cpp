5e0f1be53b1b5835df36267ad0b433d4d3f617d9
Bug0s <69805568+Bug0s@users.noreply.github.com>
2024-06-02 14:00:45 +0200

    Majdnem teljesen kész kód
    Acer és Benq projektorokra készek a műveletek, LED vezérlést kell kitalálni, hogy hogyan legyen, és azt onnantól pár sor belekódolni.
    NEC projektor azóta sem bírtam működésre, de először legyen kész és felszerelve az acer és a benq.

--- src/commands.cpp
+++ src/commands.cpp
@@ -6,15 +6,17 @@ Projector::Projector(ProjectorType projectoryType)
     switch (projectoryType)
     {
     case nec:
-        turnOnCodes = Command(turnOnNecCodes, 6);
-        turnOffCodes = Command(turnOffNecCodes, 6);
-        sourceCodes = Command(sourceNecCode, 8);
+        turnOnCodes = Command(turnOnNecCodes, 12);
+        turnOffCodes = Command(turnOffNecCodes, 12);
+        sourceCodes = Command(sourceNecCode, 12);
+        //Mi legyen a 4. funkció?
         break;
 
     case acer:
         turnOnCodes = Command(turnOnAcerCodes, 11);
         turnOffCodes = Command(turnOffAcerCodes, 11);
         sourceCodes = Command(sourceAcerCodes, 11);
+        source2Codes = Command(freezeAcerCodes, 11);
         break;
     
     case benq:
@@ -33,14 +35,7 @@ void Projector::printHexToSerial(Command com)
 {
     for (int i = 0; i < com.length; i++)
     {
-        Serial.print(com.command[i]);
+        Serial.write(com.command[i]);
     }
 }
-// NEC
-/*
-The checksum is calculated as follows.
-① Add all preceding bytes of data.
-② Use the value of the low-order one byte (eight bits) of the addition
-result obtained in ① as the checksum.
-*/
-// INFO: Ezeket lehet hogy 0x0D vel kell kezdeni hogy működjön, le kell tesztelni
+
--- src/main.cpp
+++ src/main.cpp
@@ -12,19 +12,22 @@ enum FunctionTypes
 int functionPins[] = {PIN_PC3, PIN_PC4, PIN_PC2, PIN_PC5}; // gombok
 int ledPins[] = {PIN_PB1, PIN_PD6, PIN_PB2, PIN_PD5};      // ledek
 
-FunctionTypes readInput(bool lastStatus)
+FunctionTypes readInput(bool& lastStatus)
 {
     for (int i = 0; i < 4; i++)
     {
-        if (!digitalRead(functionPins[i]))
-        { // 0-ban aktívak a bemenetek!
-            if (!lastStatus)
-            {
-                lastStatus = true;
-                return FunctionTypes(functionPins[i]); // Első gomnyomás
+        if (!digitalRead(functionPins[i])) // 0-ban aktívak a bemenetek!
+        {
+            delay(50); //Pergésmentesítés
+            if(!digitalRead(functionPins[i])) {
+                if (!lastStatus)
+                {
+                    lastStatus = true;
+                    return FunctionTypes(functionPins[i]); // Első gomnyomás
+                }
+                else
+                    return FunctionTypes(0); // Ha volt gombnyomás de már a sokadik
             }
-            else
-                return FunctionTypes(0); // Ha volt gombnyomás de már a sokadik
         }
     }
     lastStatus = false;
@@ -56,7 +59,7 @@ void setup()
     setupLedPins();
 }
 
-Projector p1(benq);
+Projector p1(acer);
 
 bool lastStatus = false;
 void loop()
@@ -64,13 +67,13 @@ void loop()
     switch (readInput(lastStatus))
     {
     case powerOn:
-        digitalWrite(PIN_PD5, HIGH);
+        //digitalWrite(PIN_PD5, HIGH); ide lehet írni led vezérléseket, ki kell találni, hogy mi legyen
         p1.printHexToSerial(p1.turnOnCodes);
         break;
 
     case powerOff:
         p1.printHexToSerial(p1.turnOffCodes);
-        digitalWrite(PIN_PD5, LOW);
+        //digitalWrite(PIN_PD5, LOW);
         break;
 
     case source:
